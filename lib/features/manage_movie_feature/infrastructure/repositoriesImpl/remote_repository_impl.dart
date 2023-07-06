79ad6f11157f7b0f448c9397568f3e19ae363dc9
phi-hotep <huguesarnold@gmail.com>
2023-07-06 17:34:42 +0100

    Remote repository Implementation

---
+++ lib/features/manage_movie_feature/infrastructure/repositoriesImpl/remote_repository_impl.dart
@@ -0,0 +1,38 @@
+import '../../../../core/error/error_imports.dart';
+import '../../application/application_imports.dart';
+import '../../domain/domain_imports.dart';
+import '../converters.dart';
+import '../infrastructure_imports.dart';
+
+class RemoteRepositoryImpl implements RemoteRepository {
+  final RemoteDataSource remoteDataSource;
+  RemoteRepositoryImpl({required this.remoteDataSource});
+  @override
+  Future<List<Movie>> getPopularMoviesDay() async {
+    try {
+      final listMovieDto = await remoteDataSource.getPopularMoviesDay();
+
+      return listMovieDto
+          .map(
+            (elt) => movieDtoToMovieConverter(elt),
+          )
+          .toList();
+    } on ServerException {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<List<Movie>> getPopularMoviesWeek() async {
+    try {
+      final listMovieDto = await remoteDataSource.getPopularMoviesWeek();
+      return listMovieDto
+          .map(
+            (elt) => movieDtoToMovieConverter(elt),
+          )
+          .toList();
+    } on ServerException {
+      rethrow;
+    }
+  }
+}
