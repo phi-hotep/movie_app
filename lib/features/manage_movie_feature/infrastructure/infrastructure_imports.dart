7ae2eb641217bb2f7a590bfcbc014f028817f477
phi-hotep <huguesarnold@gmail.com>
2023-07-06 17:35:55 +0100

    Infrastructure barel file

---
+++ lib/features/manage_movie_feature/infrastructure/infrastructure_imports.dart
@@ -0,0 +1,7 @@
+export 'repositoriesImpl/remote_repository_impl.dart';
+export 'repositoriesImpl/local_database_repository_impl.dart';
+export 'data_source/remote_data_source/remote_data_source.dart';
+export 'data_source/local_data_source/local_data_source.dart';
+export 'data_source/remote_data_source/remote_data_source_impl.dart';
+export 'data_source/local_data_source/local_data_source_impl.dart';
+export 'dtos/movie_dto.dart';
